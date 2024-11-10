efd0cb2d5a41d1703f08de88d46c0fd84504e615
gm50x <getuliomagela@outlook.com>
2024-11-10 09:27:37 -0300

    refactor: reorganizes infra project

--- apps/fiap-burger-payments/src/payment/infra/infra.module.ts
+++ apps/fiap-burger-payments/src/payment/infra/infra.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PaymentFactory } from '../application/abstractions/payment.factory';
 import { PaymentRepository } from '../application/abstractions/payment.repository';
-import { MongoosePaymentSchemaFactory } from './payment-schema.factory';
-import { MongoosePaymentFactory } from './payment.factory';
-import { MongoosePaymentRepository } from './payment.repository';
+import { MongoosePaymentSchemaFactory } from './persistance/payment-schema.factory';
+import { MongoosePaymentFactory } from './persistance/payment.factory';
+import { MongoosePaymentRepository } from './persistance/payment.repository';
 import {
   MongoosePaymentSchema,
   MongoosePaymentSchemaModel,
-} from './payment.schema';
+} from './persistance/payment.schema';
 
 @Module({
   imports: [
