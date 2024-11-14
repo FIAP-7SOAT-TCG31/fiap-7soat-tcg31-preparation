5405ab57a93863080f29950fc28a56463e033e5d
gm50x <getuliomagela@outlook.com>
2024-11-13 22:02:32 -0300

    cleanup

--- test/acceptance/steps/app.module.ts
+++ test/acceptance/steps/app.module.ts
@@ -1,9 +1,8 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { IsItFridayYetSuiteSteps } from './step-definitions/is-it-friday-yet.steps';
 import { PaymentSuite } from './step-definitions/payment.suite';
 @Module({
   imports: [HttpModule],
-  providers: [PaymentSuite, IsItFridayYetSuiteSteps],
+  providers: [PaymentSuite],
 })
 export class AppModule {}
