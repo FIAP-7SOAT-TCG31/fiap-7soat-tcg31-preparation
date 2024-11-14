f15df5b15c0a25af44f6d8eeaf6474eda2d0f0da
gm50x <getuliomagela@outlook.com>
2024-11-13 22:00:38 -0300

    feat: adds cucumber tests

--- test/acceptance/features/hello-world.feature
+++
@@ -1,8 +0,0 @@
-@HelloWorld
-Feature: Greetings
-  Everybody starts with Hello World!
-
-  Scenario: 
-    Given I am learning BDD
-    When I make an HTTP Request on the application root path
-    Then it should say "Hello World!"
--- test/acceptance/steps/app.module.ts
+++ test/acceptance/steps/app.module.ts
@@ -1,9 +1,9 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { HelloWorldSuite } from './step-definitions/hello-world.suite';
 import { IsItFridayYetSuiteSteps } from './step-definitions/is-it-friday-yet.steps';
+import { PaymentSuite } from './step-definitions/payment.suite';
 @Module({
   imports: [HttpModule],
-  providers: [HelloWorldSuite, IsItFridayYetSuiteSteps],
+  providers: [PaymentSuite, IsItFridayYetSuiteSteps],
 })
 export class AppModule {}
