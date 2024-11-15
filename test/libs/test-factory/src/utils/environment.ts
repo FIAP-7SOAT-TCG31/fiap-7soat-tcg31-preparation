fbd677a90b2993921c28a5eeaaa99d54a1d4a428
gm50x <getuliomagela@outlook.com>
2024-11-15 12:36:01 -0300

    use localhost

--- test/libs/test-factory/src/utils/environment.ts
+++ test/libs/test-factory/src/utils/environment.ts
@@ -1,7 +1,7 @@
 import { randomUUID } from 'crypto';
 
-const rabbitmqHost = process.env.CI ? 'fiapburger-rabbitmq' : 'localhost';
-const mongodbHost = process.env.CI ? 'fiapburger-mongodb' : 'localhost';
+const rabbitmqHost = 'localhost';
+const mongodbHost = 'localhost';
 
 const basicBearer = `fiapburger:fiapburger`;
 export const virtualEnvironment = randomUUID().split('-').at(0);
