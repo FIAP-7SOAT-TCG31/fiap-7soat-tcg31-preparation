57774cd233d17ddce9d66b6483e3ef7a0f7c8efa
gm50x <getuliomagela@outlook.com>
2024-11-15 12:14:03 -0300

    try changing hosts for CI

--- test/libs/test-factory/src/utils/environment.ts
+++ test/libs/test-factory/src/utils/environment.ts
@@ -1,11 +1,14 @@
 import { randomUUID } from 'crypto';
 
+const rabbitmqHost = process.env.CI ? 'rabbitmq' : 'localhost';
+const mongodbHost = process.env.CI ? 'mongodb' : 'localhost';
+
 const basicBearer = `fiapburger:fiapburger`;
 export const virtualEnvironment = randomUUID().split('-').at(0);
-export const rabbitmqURL = `http://${basicBearer}@localhost:15672`;
+export const rabbitmqURL = `http://${basicBearer}@${rabbitmqHost}:15672`;
 
 export const environment = {
   NODE_ENV: 'testing',
-  MONGO_URL: `mongodb://${basicBearer}@localhost:27017/${virtualEnvironment}?authSource=admin`,
-  AMQP_URL: `amqp://${basicBearer}@localhost:5672/${virtualEnvironment}`,
+  MONGO_URL: `mongodb://${basicBearer}@${mongodbHost}:27017/${virtualEnvironment}?authSource=admin`,
+  AMQP_URL: `amqp://${basicBearer}@${rabbitmqHost}:5672/${virtualEnvironment}`,
 };
