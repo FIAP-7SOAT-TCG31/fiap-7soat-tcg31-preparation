e39ff1048de985d24b5fac97f6d7d9f1cd758a59
gm50x <getuliomagela@outlook.com>
2024-11-19 08:14:01 -0300

    fix: incorrect method in advance preparation controller

--- apps/fiap-burger-preparation/src/preparation/drivers/advance-preparation.controller.ts
+++ apps/fiap-burger-preparation/src/preparation/drivers/advance-preparation.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Param, Post } from '@nestjs/common';
+import { Controller, Param, Patch } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { AdvancePreparationCommand } from '../application/commands/adavance-preparation.command';
 import { ObjectIdValidationPipe } from '../infra/pipes/object-id-validation.pipe';
@@ -7,7 +7,7 @@ import { ObjectIdValidationPipe } from '../infra/pipes/object-id-validation.pipe
 export class AdvancePreparationController {
   constructor(private readonly commandBus: CommandBus) {}
 
-  @Post(':id/advance')
+  @Patch(':id/advance')
   async execute(@Param('id', new ObjectIdValidationPipe()) id: string) {
     await this.commandBus.execute(new AdvancePreparationCommand(id));
   }
