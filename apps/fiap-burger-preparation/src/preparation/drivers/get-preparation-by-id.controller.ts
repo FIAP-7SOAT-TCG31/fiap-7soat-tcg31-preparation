8fa0727e90541a890fcf173af122dd463de9756d
gm50x <getuliomagela@outlook.com>
2024-11-23 18:51:15 -0300

    feat: adds id validation

--- apps/fiap-burger-preparation/src/preparation/drivers/get-preparation-by-id.controller.ts
+++ apps/fiap-burger-preparation/src/preparation/drivers/get-preparation-by-id.controller.ts
@@ -4,13 +4,14 @@ import {
   GetPreparationByIdQuery,
   GetPreparationByIdResult,
 } from '../application/queries/get-preparation-by-id.query';
+import { UUIDValidationPipe } from '../infra/pipes/uuid-validator.pipe';
 
 @Controller({ version: '1', path: 'preparations' })
 export class GetPreparationByIdController {
   constructor(private readonly queryBus: QueryBus) {}
 
   @Get(':id')
-  async execute(@Param('id') id: string) {
+  async execute(@Param('id', new UUIDValidationPipe()) id: string) {
     const result = await this.queryBus.execute<
       GetPreparationByIdQuery,
       GetPreparationByIdResult
